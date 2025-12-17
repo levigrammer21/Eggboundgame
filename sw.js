875c7cac65f2821de02991eba036726389e13f9d
levigrammer21 <levigrammer@gmail.com>
2025-12-17 11:36:30 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="eggbound-clean-v2";
-const ASSETS=["./","./index.html","./manifest.json"];
+const CACHE="eggbound-polished-v2";
+const ASSETS=["./","./index.html","./manifest.json","./sw.js"];
 
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
@@ -8,15 +8,13 @@ self.addEventListener("install",e=>{
 
 self.addEventListener("activate",e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch",e=>{
-  if(e.request.url.startsWith(self.location.origin)){
-    e.respondWith(
-      caches.match(e.request).then(r=>r||fetch(e.request))
-    );
-  }
+  const url = new URL(e.request.url);
+  if(url.origin !== location.origin) return; // don't cache PokéAPI
+  e.respondWith(caches.match(e.request, {ignoreSearch:true}).then(r=>r||fetch(e.request)));
 });
